5be6fdd6233fba5a5c1420c3dd42327d2df01e6c
dvir613 <dvirbd613@gmail.com>
2024-08-22 16:14:50 +0300

    added a file for preparing the data for the models to make the scripts cleaner

--- data_process/EMG_to_Avatar_model.py
+++ data_process/EMG_to_Avatar_model.py
@@ -17,6 +17,8 @@ from sklearn.ensemble import ExtraTreesRegressor
 from scipy.stats import ttest_rel
 import seaborn as sns
 
+from prepare_data_for_model import *
+
 
 # Get the absolute path of the current script
 script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -27,23 +29,6 @@ test_eeg = False
 save_results = True
 segments_length = 35  # length of each segment in seconds
 models = ['LR', 'ETR', 'Ridge']
-
-
-def sliding_window(data, method, window_size=50, step_size=25):
-    # Calculate number of windows
-    num_windows = (data.shape[1] - window_size) // step_size + 1
-    # print(num_windows)
-    result = np.zeros((data.shape[0], num_windows))
-    if method == "RMS":
-        data = data ** 2
-        for i in range(data.shape[0]):
-            for j in range(num_windows):
-                result[i, j] = np.sqrt(np.mean(data[i, j * step_size:j * step_size + window_size]))
-    if method == "MEAN":
-        for i in range(data.shape[0]):
-            for j in range(num_windows):
-                result[i, j] = np.mean(data[i, j * step_size:j * step_size + window_size])
-    return result
 
 
 # Function to evaluate models and print results
@@ -128,16 +113,7 @@ for participant_folder in os.listdir(data_path):
     for session_folder in os.listdir(participant_folder_path):
         session_folder_path = fr'{participant_folder_path}\{session_folder}'
         session_number = session_folder
-        for file in os.listdir(session_folder_path):
-            if file == fr"{participant_ID}_{session_number}_db15_Y.npy":
-                ica_before_order = np.load(f"{session_folder_path}/{file}")
-            if file == fr"{participant_ID}_{session_number}_db15_electrode_order.npy":
-                electrode_order = np.load(f"{session_folder_path}/{file}")
-            continue
-        ica_after_order = np.zeros_like(ica_before_order, dtype=float)
-        for i in range(16):
-            if int(electrode_order[i]) != 16:
-                ica_after_order[electrode_order[i], :] = ica_before_order[i, :]
+        ica_after_order = extract_and_order_ica_data(participant_ID, session_folder_path, session_number)
         # print(ica_after_order.shape)
 
         edf_path = fr"{session_folder_path}\{participant_ID}_{session_number}.edf"
@@ -150,99 +126,42 @@ for participant_folder in os.listdir(data_path):
 
         X_full = ica_after_order
         # normalize
-        X_full = X_full / (np.mean(np.abs(X_full), axis=1).reshape(-1, 1)
-                           + 4 * np.std(np.abs(X_full), axis=1).reshape(-1, 1) + 1e-7)  # added epsilon to avoid division by zero
+        X_full = normalize_ica_data(X_full)
         print("X_full shape: ", X_full.shape)
+
         # RMS
-        X_full_RMS = sliding_window(X_full, method="RMS", window_size=50, step_size=25).T
+        X_full_RMS = sliding_window(X_full, method="RMS", fs=emg_fs).T
+
+        annotations_list = ['05_Forehead', '07_Eye_gentle', '09_Eye_tight', '12_Nose', '14_Smile_closed',
+                            '16_Smile_open', '19_Lip_pucker', '21_Cheeks', '23_Snarl', '26_Depress_lip']
+        events_timings = get_annotations_timings(emg_file, annotations_list)
+
+        participant_ica_windows = prepare_relevant_data(ica_after_order, emg_file, emg_fs, events_timings=events_timings,
+                                                        segments_length=segments_length, norm="ICA", averaging="RMS")
 
         # add EEG
         if test_eeg:
-            eeg = np.zeros((2, ica_after_order.shape[1]))
-            eeg[0, :] = mne.filter.filter_data(emg_file.get_data()[14], emg_fs, l_freq=0.3, h_freq=30, method='fir', copy=True)
-            eeg[1, :] = mne.filter.filter_data(emg_file.get_data()[15], emg_fs, l_freq=0.3, h_freq=30, method='fir', copy=True)
-            ica_after_order_with_eeg = np.concatenate((ica_after_order, eeg), axis=0)
+            participant_ica_windows_with_eeg = add_eeg(emg_file, participant_ica_windows, fs=emg_fs, events_timings=events_timings,
+                                                        segments_length=segments_length, norm="ICA", averaging="RMS")
 
-        # Load the annotations
-        edf_annotations = emg_file.annotations
-        events_annotations = ['05_Forehead', '07_Eye_gentle', '09_Eye_tight', '12_Nose', '14_Smile_closed', '16_Smile_open',
-                              '19_Lip_pucker', '21_Cheeks', '23_Snarl', '26_Depress_lip']
-        event_annotations = [annot for annot in edf_annotations if annot['description'] in events_annotations]
-        event_timings = [annot['onset'] for annot in event_annotations]
-
-        # splice the data to contain only the events segments
-        ica_relevant_data = np.concatenate([ica_after_order[:, int((timing-5)*emg_fs):int((timing+segments_length-5)*emg_fs)] for timing in event_timings], axis=1)
-        # print(np.mean(ica_relevant_data, axis=1), np.max(ica_relevant_data, axis=1), np.min(ica_relevant_data, axis=1))
-        # normalize according to mean + 3 std to avoid outliers interference
-        ica_relevant_data_norm = ica_relevant_data / (np.mean(np.abs(ica_relevant_data), axis=1).reshape(-1, 1)
-                                                 + 4*np.std(np.abs(ica_relevant_data), axis=1).reshape(-1, 1) + 1e-7)  # added epsilon to avoid division by zero
-        # print(np.mean(ica_relevant_data_norm, axis=1), np.max(ica_relevant_data_norm, axis=1), np.min(ica_relevant_data_norm, axis=1))
-        print("ica relevant data shape: ", ica_relevant_data.shape)
-        participant_ica_windows = sliding_window(ica_relevant_data_norm, method="RMS", window_size=50, step_size=25)
-
-        if test_eeg:
-            ica_relevant_data_with_eeg = np.concatenate([ica_after_order_with_eeg[:, int((timing-5)*emg_fs):int((timing+segments_length-5)*emg_fs)] for timing in event_timings], axis=1)
-            # normalize the data between -1 and 1
-            ica_relevant_data_with_eeg = ica_relevant_data_with_eeg / np.max(np.abs(ica_relevant_data_with_eeg), axis=1).reshape(-1, 1)
-            print("ica relevant data with eeg shape: ", ica_relevant_data_with_eeg.shape)
-            participant_ica_windows_with_eeg = sliding_window(ica_relevant_data_with_eeg, method="RMS", window_size=50, step_size=25)
-
-        # get edf start time
-        if participant_ID == 'participant_01':
-            edf_start_time = emg_file.info['meas_date']
-            edf_start_time = edf_start_time.time()
-        else:
-            # the last annotation (before "stop recording") is: "data.start_time: YYYY-MM-DD HH:MM:SS.fff"
-            edf_start_time = edf_annotations[-2]['description'].split(' ')[-1]
-            edf_start_time = datetime.strptime(edf_start_time, "%H:%M:%S.%f")
-            edf_start_time = edf_start_time.time()
 
         # Load the avatar data skip the first row (the features names)
         avatar_data = pd.read_csv(fr"{session_folder_path}/"
                                   fr"{participant_ID}_{session_number}_interpolated_relevant_only_right.csv",
                                   header=0, index_col=0)
-        avatar_start_time = avatar_data.index[0]
-        avatar_start_time = datetime.strptime(avatar_start_time, "%H:%M:%S.%f")
-        avatar_start_time = avatar_start_time.time()
-
-        # Calculate the time delta ignoring the date
-        edf_start_seconds = timedelta(hours=edf_start_time.hour,
-                                      minutes=edf_start_time.minute,
-                                      seconds=edf_start_time.second,
-                                      microseconds=edf_start_time.microsecond).total_seconds()
-
-        avatar_start_seconds = timedelta(hours=avatar_start_time.hour,
-                                         minutes=avatar_start_time.minute,
-                                         seconds=avatar_start_time.second,
-                                         microseconds=avatar_start_time.microsecond).total_seconds()
-
-        time_delta = edf_start_seconds - avatar_start_seconds
-        print("time difference: ", int(time_delta * 1000), "milliseconds")
 
         blendshapes = avatar_data.columns
-        avatar_data = avatar_data.to_numpy().T
-        print("original avatar data shape: ", avatar_data.shape)
-        avatar_fps = 60  # data collection was in 60 fps
-        frames_to_cut = int(time_delta * avatar_fps)
-        avatar_data = avatar_data[:, frames_to_cut:]
-        print("avatar data cut shape: ", avatar_data.shape)
 
-        avatar_relevant_data = np.concatenate([avatar_data[:, int(timing*avatar_fps):int((timing+segments_length)*avatar_fps)] for timing in event_timings], axis=1)
-        print("avatar data shape after cut and splice: ", avatar_relevant_data.shape)
+        participant_avatar_windows_cut = prepare_avatar_relevant_data(participant_ID, avatar_data, emg_file, participant_ica_windows,
+                                                                      fs=60, events_timings=events_timings,
+                                                                      segments_length=segments_length, norm=None, averaging="MEAN")
 
-        participant_avatar_windows = sliding_window(avatar_relevant_data, method="MEAN", window_size=6, step_size=3)
-        print("avatar windows shape before crop:", participant_avatar_windows.shape)
-
-        # Cut avatar windows to match the number of ICA windows
-        participant_avatar_windows_cut = participant_avatar_windows[:, :participant_ica_windows.shape[1]]
-
-        print("ica windows shape:", participant_ica_windows.shape, "avatar windows shape:", participant_avatar_windows_cut.shape)
 
         # model
 
         # Transpose to get the shape (samples, features)
-        X_ica = participant_ica_windows.T  # Shape: (11679, 16)
-        Y = participant_avatar_windows_cut.T  # Shape: (11679, 33)
+        X_ica = participant_ica_windows.T
+        Y = participant_avatar_windows_cut.T
 
         # # Impute NaNs in Y data
         # imputer = SimpleImputer(strategy='constant', fill_value=0)
@@ -266,9 +185,9 @@ for participant_folder in os.listdir(data_path):
 
             model_path = fr"{session_folder_path}/{participant_ID}_{session_number}_blendshapes_model_{model}.joblib"
 
-            # if os.path.exists(model_path):  # Check if the model already exists
-            #     print(f"Model {model} already exists. Skipping to the next model...")
-            #     continue  # Skip the current model and continue with the next one
+            if os.path.exists(model_path):  # Check if the model already exists
+                print(f"Model {model} already exists. Skipping to the next model...")
+                continue  # Skip the current model and continue with the next one
 
             # Evaluate models for ICA data
             model_ica, mse_ica, r2_ica, mae_ica, Y_pred_ica = evaluate_models(X_ica_train, X_ica_test, Y_train, Y_test,
@@ -309,7 +228,7 @@ for participant_folder in os.listdir(data_path):
 
         # save participant avatar windows data as csv to compare to predicted data
         avatar_sliding_window_method = "MEAN"
-        full_avatar_data_windows = sliding_window(avatar_data, method=avatar_sliding_window_method, window_size=6, step_size=3).T
+        full_avatar_data_windows = sliding_window(avatar_data, method=avatar_sliding_window_method, fs=60).T
         print("full avatar windows shape before crop:", full_avatar_data_windows.shape)
         # make it the same size as the predicted values
         full_avatar_data_windows = full_avatar_data_windows[:X_full_RMS.shape[0], :]
---
+++ data_process/prepare_data_for_model.py
@@ -0,0 +1,158 @@
+import mne
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from datetime import datetime, timezone, timedelta
+
+
+# Get the absolute path of the current script
+script_dir = os.path.dirname(os.path.abspath(__file__))
+# Define the project folder by going up two levels from the script's directory
+project_folder = os.path.abspath(os.path.join(script_dir, '..'))
+
+
+# window length and step length in seconds
+def sliding_window(data, method, fs, window_length=0.1, step_length=0.05):
+    window_size = int(window_length * fs)
+    step_size = int(step_length * fs)
+    # Calculate number of windows
+    num_windows = (data.shape[1] - window_size) // step_size + 1
+    # print(num_windows)
+    result = np.zeros((data.shape[0], num_windows))
+    if method == "RMS":
+        data = data ** 2
+        for i in range(data.shape[0]):
+            for j in range(num_windows):
+                result[i, j] = np.sqrt(np.mean(data[i, j * step_size:j * step_size + window_size]))
+    if method == "MEAN":
+        for i in range(data.shape[0]):
+            for j in range(num_windows):
+                result[i, j] = np.mean(data[i, j * step_size:j * step_size + window_size])
+    return result
+
+
+def extract_and_order_ica_data(participant_ID, session_folder_path, session_number):
+    ica_before_order= None
+    electrode_order = None
+    for file in os.listdir(session_folder_path):
+        if file == fr"{participant_ID}_{session_number}_db15_Y.npy":
+            ica_before_order = np.load(f"{session_folder_path}/{file}")
+        if file == fr"{participant_ID}_{session_number}_db15_electrode_order.npy":
+            electrode_order = np.load(f"{session_folder_path}/{file}")
+        continue
+    if ica_before_order and electrode_order:
+        ica_after_order = np.zeros_like(ica_before_order, dtype=float)
+        for i in range(16):
+            if int(electrode_order[i]) != 16:
+                ica_after_order[electrode_order[i], :] = ica_before_order[i, :]
+
+        return ica_after_order
+    else:
+        print("Error: ICA data or electrode order not found")
+        return None
+
+
+def normalize_ica_data(ica_data):
+    # Normalize the data to include 99% of the data in the range [-1, 1] (avoiding outliers)
+    ica_data = ica_data / (np.mean(np.abs(ica_data), axis=1).reshape(-1, 1)
+                           + 4 * np.std(np.abs(ica_data), axis=1).reshape(-1, 1) + 1e-7)
+    # added epsilon to avoid division by zero
+    return ica_data
+
+
+def get_annotations_timings(emg_file, annotations_list=None):
+    if annotations_list is None:
+        annotations_list = ['05_Forehead', '07_Eye_gentle', '09_Eye_tight', '12_Nose', '14_Smile_closed',
+                            '16_Smile_open', '19_Lip_pucker', '21_Cheeks', '23_Snarl', '26_Depress_lip']
+    edf_annotations = emg_file.annotations
+    events_annotations = [annot for annot in edf_annotations if annot['description'] in annotations_list]
+    events_timings = [annot['onset'] for annot in events_annotations]
+    return events_timings
+
+
+def prepare_relevant_data(data, emg_file, fs, events_timings=None, segments_length=35, norm=None, averaging="RMS"):
+    if events_timings is None:
+        events_timings = get_annotations_timings(emg_file)
+    # splice the data to contain only the events segments
+    relevant_data = np.concatenate(
+        [data[:, int((timing - 5) * fs):int((timing + segments_length - 5) * fs)] for timing in
+         events_timings], axis=1)
+    if norm == "ICA":
+        relevant_data = normalize_ica_data(relevant_data)
+    print("relevant data shape: ", relevant_data.shape)
+    # sliding window averaging
+    relevant_data_averaged = sliding_window(relevant_data, method=averaging, fs=fs)
+
+    return relevant_data_averaged
+
+
+def prepare_avatar_relevant_data(participant_ID, avatar_data, emg_file, emg_relevant_data_averaged, fs=60, events_timings=None, segments_length=35, norm=None, averaging="MEAN"):
+    time_delta = get_time_delta(emg_file, avatar_data, participant_ID)
+    avatar_data = avatar_data.to_numpy().T
+    print("original avatar data shape: ", avatar_data.shape)
+    avatar_fps = fs  # data collection was in 60 fps
+    frames_to_cut = int(time_delta * avatar_fps)
+    avatar_data = avatar_data[:, frames_to_cut:]
+    print("avatar data cut shape: ", avatar_data.shape)
+
+    avatar_relevant_data_averaged =  prepare_relevant_data(avatar_data, emg_file=emg_file, fs=fs, events_timings=events_timings,
+                                                           segments_length=segments_length, norm=norm, averaging=averaging)
+    print("avatar windows shape before crop:", avatar_relevant_data_averaged.shape)
+
+    # Cut avatar windows to match the number of ICA windows
+    avatar_relevant_data_averaged_cut = avatar_relevant_data_averaged[:, :emg_relevant_data_averaged.shape[1]]
+
+    print("ica windows shape:", emg_relevant_data_averaged.shape, "avatar windows shape:",
+          avatar_relevant_data_averaged_cut.shape)
+    return avatar_relevant_data_averaged_cut
+
+
+def get_time_delta(emg_file, avatar_file, participant_ID):
+    # get edf start time
+    if participant_ID == 'participant_01':
+        edf_start_time = emg_file.info['meas_date']
+        edf_start_time = edf_start_time.time()
+    else:
+        # the last annotation (before "stop recording") is: "data.start_time: YYYY-MM-DD HH:MM:SS.fff"
+        edf_annotations = emg_file.annotations
+        edf_start_time = edf_annotations[-2]['description'].split(' ')[-1]
+        edf_start_time = datetime.strptime(edf_start_time, "%H:%M:%S.%f")
+        edf_start_time = edf_start_time.time()
+
+    # get avatar start time
+    avatar_start_time = avatar_file.index[0]
+    avatar_start_time = datetime.strptime(avatar_start_time, "%H:%M:%S.%f")
+    avatar_start_time = avatar_start_time.time()
+
+    # Calculate the time delta ignoring the date
+    edf_start_seconds = timedelta(hours=edf_start_time.hour,
+                                  minutes=edf_start_time.minute,
+                                  seconds=edf_start_time.second,
+                                  microseconds=edf_start_time.microsecond).total_seconds()
+
+    avatar_start_seconds = timedelta(hours=avatar_start_time.hour,
+                                     minutes=avatar_start_time.minute,
+                                     seconds=avatar_start_time.second,
+                                     microseconds=avatar_start_time.microsecond).total_seconds()
+
+    time_delta = edf_start_seconds - avatar_start_seconds
+    print("time difference: ", int(time_delta * 1000), "milliseconds")
+    return time_delta
+
+
+def add_eeg(emg_file, emg_relevant_data_averaged, fs, events_timings, segments_length=35, norm="ICA", averaging="RMS"):
+    eeg = np.zeros((2, emg_relevant_data_averaged.shape[1]))
+    eeg[0, :] = mne.filter.filter_data(emg_file.get_data()[14], fs, l_freq=0.3, h_freq=30, method='fir', copy=True)
+    eeg[1, :] = mne.filter.filter_data(emg_file.get_data()[15], fs, l_freq=0.3, h_freq=30, method='fir', copy=True)
+    # get relevant normalized averaged data
+    eeg_relevant_data_averaged = prepare_relevant_data(eeg, emg_file=emg_file, fs=fs, events_timings=events_timings,
+                                                       segments_length=35, norm="ICA", averaging="RMS")
+    # concatenate
+    relevant_data = np.concatenate((emg_relevant_data_averaged, eeg_relevant_data_averaged), axis=0)
+    return relevant_data
+
+
+
+
+
