3b7f04a9b9295c9f7d7ae66c7536747e4296b09c
hilaman1 <hilaman1@mail.tau.ac.il>
2024-11-15 16:15:46 +0200

    change in experiment.py

--- experiment/experiment.py
+++ experiment/experiment.py
@@ -17,17 +17,17 @@ def play_videos(directory, data, trial_number):
     # Loop through each video file
     for video_file in video_files:
         video_path = os.path.join(directory, video_file)
-        # TODO remove the following lines
-        if '02_Introduction' in video_file:
-            continue
-        if '01_Info' in video_file:
-            continue
-        if '37_Credits' in video_file:
-            continue
-        if '36_Natural_smile' in video_file:
-            continue
-        if 'demonstration' in video_file:
-            continue
+        # # TODO remove the following lines
+        # if '02_Introduction' in video_file:
+        #     continue
+        # if '01_Info' in video_file:
+        #     continue
+        # if '37_Credits' in video_file:
+        #     continue
+        # if '36_Natural_smile' in video_file:
+        #     continue
+        # if 'demonstration' in video_file:
+        #     continue
 
         annotation = video_file.split('.')[0]
         data.add_annotation(annotation + f"_trial_{trial_number}")
@@ -78,8 +78,8 @@ if __name__ == '__main__':
     host_name = "127.0.0.1"
 
     data_path = r"C:\Users\YH006_new\fEMG_to_avatar\data"
-    participant_ID = "participant_03"
-    session_number = 2
+    participant_ID = "participant_04"
+    session_number = 1
     participant_folder = os.path.join(data_path, participant_ID)
     session_folder = os.path.join(participant_folder, f"S{session_number}")
     if not os.path.exists(participant_folder):
@@ -95,10 +95,11 @@ if __name__ == '__main__':
 
     data.add_annotation("Start recording")
     directory = 'experiment videos'
-    for trial_number in range(1, 4):
+    for trial_number in range(1, 2):
         play_videos(directory, data, str(trial_number))
 
     free_behavior()
+
 
     data.add_annotation("data_start_time: " + str(data.start_time))
 
